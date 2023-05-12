e84c1a0528524611d2901f19429de4494a9d0cb5
Merus1999 <133385886+Merus1999@users.noreply.github.com>
2023-05-13 00:00:35 +0700

    Add files via upload
    đientich

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class HelloWorld {
+  static void Main() {
+    float width;
+    float height;
+    Console.Write("Enter width: ");
+    width = float.Parse(Console.ReadLine());
+    Console.Write("Enter height: ");
+    height = float.Parse(Console.ReadLine()); 
+    float area = width * height;
+    Console.WriteLine("Area is: " + area);
+  }
+}
