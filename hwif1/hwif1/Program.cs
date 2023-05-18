5396d01474ca6e4071770f8197a82db8f3b2001c
Merus1999 <133385886+Merus1999@users.noreply.github.com>
2023-05-18 10:12:23 +0700

    Add files via upload
    ///bài tập về nhà if (code gym )

---
+++ hwif1/hwif1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hwif1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("nhap a: ");
+            a = int.Parse(Console.ReadLine());
+            int b;
+            Console.WriteLine("nhap b: ");
+            b = int.Parse(Console.ReadLine());
+            if (a == 0)
+            {
+                Console.WriteLine("phuong trinh ko co nghiem x = -b/a");
+            }
+            else if (a != 0 && b == 0)
+            {
+                Console.WriteLine("phuong trinh co vo so nghiem x");
+            }
+            else
+            {
+                Console.WriteLine("phuong trinh co 1 nghiem x = -b/a");
+                Console.WriteLine(-b / a);
+            }
+            Console.ReadKey();
+        }
+    }
+}
