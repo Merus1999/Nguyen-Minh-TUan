3217de9066968bc35430f6247f830455af3c4abb
Merus1999 <133385886+Merus1999@users.noreply.github.com>
2023-05-18 10:40:21 +0700

    Add files via upload
    // bài tập câu đk 2 code gym

---
+++ hwif2/hwif2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hwif2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double height;
+            double weight;
+            Console.WriteLine("nhap chieu cao: ");
+            height = double.Parse(Console.ReadLine());
+            Console.WriteLine("nhap can nang: ");
+            weight = double.Parse(Console.ReadLine());
+            //bmi là chí số cơ thể ==> công thức bmi = weight/height^2
+            double bmi = weight / (height*height);//có thể dùng math.pow là công thức tính mũ math.pow(ten_bien,số lần mũ)
+            bmi = Math.Round(bmi, 1);//math.roung là công thức làm tròn 
+            Console.WriteLine("BMI: " + bmi);
+            if (bmi < 18)
+            {
+                Console.WriteLine(" Underweight");
+            }
+            else if (bmi < 25.0)
+            {
+                Console.WriteLine(" Normal");
+            }
+            else if (bmi < 30.0)
+            {
+                Console.WriteLine(" Overweight");
+            }
+            else
+            {
+                Console.WriteLine(" Obese");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
