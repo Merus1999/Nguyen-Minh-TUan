8b92e49789dd34b708cf063a6be968379e91c006
Merus1999 <133385886+Merus1999@users.noreply.github.com>
2023-05-22 10:24:02 +0700

    Add files via upload
    số nguyên tố

---
+++ ktsonguyento/ktsonguyento/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ktsonguyento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Moi ban nhap so n= ");
+            int dem = 0; //đếm số ước số
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0) dem++;
+            }
+            if (dem == 2) Console.WriteLine(n + " la SNT");
+            else Console.WriteLine(n + " ko la SNT");
+            Console.ReadKey();
+        
+
+    }
+}
+}
